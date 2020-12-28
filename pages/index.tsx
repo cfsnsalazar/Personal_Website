0ce767ac7c4c85b02d1ce60e8cf63a2858a0dcc6
Napoleon Salazar <nsalazar0429@gmail.com>
2020-12-28 13:13:45 -0500

    commit latest updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 const IndexPage: FC = () => {
-    return <h1 className="text-red-500">Hello, CodeChem!</h1>;
+    return <h1 className="text-5xl text-red-500">Hello, CodeChem!</h1>;
 };
 
 export default IndexPage;
